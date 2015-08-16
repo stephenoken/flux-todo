f15415380af5365f73c37a40e2a7a8affee128c3
Stephen O'Kennedy <stephenoken@gmail.com>
2015-08-16 13:27:26 +0100

    Added all tests on the TodoDispatcher object

--- test/TodoDispatcher-test.js
+++ test/TodoDispatcher-test.js
@@ -20,4 +20,61 @@ describe('TodoDispatcher', function () {
     TodoDispatcher.dispatch(payload);
     expect(listener).to.be.calledOnce;
   });
+  it('waits with chained dependancies properly', function () {
+    var payload = {};
+
+    var listener1Done = false;
+    var listener1 = function (pl) {
+      TodoDispatcher.waitFor([index2,index4]);
+      expect(listener2Done).to.be.true;
+      expect(listener3Done).to.be.true;
+      expect(listener4Done).to.be.true;
+      listener1Done = true;
+    };
+
+    var index1 = TodoDispatcher.register(listener1);
+
+    var listener2Done = false;
+    var listener2 = function (pl) {
+      TodoDispatcher.waitFor([index3]);
+      expect(listener3Done).to.be.true;
+      listener2Done = true;
+    };
+
+    var index2 = TodoDispatcher.register(listener2);
+
+    var listener3Done = false;
+    var listener3 = function (pl) {
+      listener3Done = true;
+    };
+
+    var index3 = TodoDispatcher.register(listener3);
+
+    var listener4Done = false;
+    var listener4 = function (pl) {
+      TodoDispatcher.waitFor([index3]);
+      expect(listener3Done).to.be.true;
+      listener4Done = true;
+    };
+
+    var index4 = TodoDispatcher.register(listener4);
+
+    var TodoDispatcherLifeCycleAPI = {
+      runs: (function () {
+        TodoDispatcher.dispatch(payload);
+      }),
+      waitsFor: (function () {
+        return listener1Done;
+      }),
+      runAll: (function () {
+        expect(listener1Done).to.be.true;
+        expect(listener2Done).to.be.true;
+        expect(listener3Done).to.be.true;
+      })
+    };
+    TodoDispatcherLifeCycleAPI.runs();
+    TodoDispatcherLifeCycleAPI.waitsFor();
+    TodoDispatcherLifeCycleAPI.runAll();
+  });
+
 });
